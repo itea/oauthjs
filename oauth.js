ef6a37ffe6ef152a39498232d7c335db3838f6c0
itea <itea@itea-ubuntu.(none)>
2010-10-29 22:29:19 +0800

    initialize commit

---
+++ oauth.js
@@ -0,0 +1,16 @@
+/**
+ * oauth.js
+ * OAuth Client for Node.js
+ * @author itea 2010-10-29
+ * @version 0.1 2010-10-29
+ */
+
+function OAuth(option) {
+}
+
+OAuth.prototype.getRequestToken = function(){};
+
+OAuth.prototype.authorizeToken = function(){};
+
+OAuth.prototype.getAccessToken = function(){};
+
